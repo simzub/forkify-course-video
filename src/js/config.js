c88b7e475a7dbfb7ecff89f771be793b48ac3a1d
simzub <szubkonis@gmail.com>
2022-06-05 10:19:36 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9f41a5aa-97e9-4b61-95e8-26f04293a78f';
+export const MODAL_CLOSE_SEC = 2.5;
